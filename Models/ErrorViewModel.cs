25d2c81234e549e19719b54fad278fe0508b9495
Niruxaben Padhiyar <nilipadhiyar293@outlook.com>
2022-03-22 15:41:02 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
